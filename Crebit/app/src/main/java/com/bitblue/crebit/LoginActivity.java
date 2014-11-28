76c245059ae431caae0bb731cf26c2dac5773518
Azhar-Ansari <aansari4u@gmail.com>
2014-11-28 19:00:29 +0530

    Added login signup and forgot password

--- Crebit/app/src/main/java/com/bitblue/crebit/LoginActivity.java
+++ Crebit/app/src/main/java/com/bitblue/crebit/LoginActivity.java
@@ -1,20 +1,83 @@
 package com.bitblue.crebit;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener {
+
+    EditText mNumber, passwd;
+    Button login, forgotPass, signUp;
+    String mobileNumber, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
         setContentView(R.layout.activity_login);
+        initViews();
+    }
+
+    public void initViews() {
+        mNumber = (EditText) findViewById(R.id.et_mobileNumber);
+        passwd = (EditText) findViewById(R.id.et_password);
+        login = (Button) findViewById(R.id.b_login);
+        forgotPass = (Button) findViewById(R.id.b_forgot_pass);
+        signUp = (Button) findViewById(R.id.b_signUp);
+/*        mNumber.setOnTouchListener(this);
+        passwd.setOnTouchListener(this);*/
+        mNumber.setOnClickListener(this);
+        passwd.setOnClickListener(this);
+        login.setOnClickListener(this);
+        forgotPass.setOnClickListener(this);
+        signUp.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.b_login:
+                if (mNumber.getText() != null && passwd.getText().toString() != null) {
+                    mobileNumber = mNumber.getText().toString();
+                    password = passwd.getText().toString();
+                }
+                break;
+
+            case R.id.b_forgot_pass:
+                Intent openForgotPass = new Intent(LoginActivity.this, ForgotPass.class);
+                startActivity(openForgotPass);
+
+                break;
+            case R.id.b_signUp:
+                Intent opensignUp = new Intent(LoginActivity.this, SignUp.class);
+                startActivity(opensignUp);
+                break;
+
+        }
+    }
+
+/*    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (view.getId()) {
+            case R.id.et_mobileNumber:
+                if (passwd.getText().toString().equals(""))
+                    passwd.setHint("  Password");
+                mNumber.setHint("");
+                mNumber.requestFocus();
+                break;
+            case R.id.et_password:
+                if (mNumber.getText().toString().equals(""))
+                    mNumber.setHint("  Mobile Number");
+                passwd.setHint("");
+                passwd.requestFocus();
+                break;
+
+        }
+        return true;
+    }*/
+
 }
--- Crebit/app/src/main/java/com/bitblue/crebit/SplashScreenActivity.java
+++ Crebit/app/src/main/java/com/bitblue/crebit/SplashScreenActivity.java
@@ -17,7 +17,7 @@ public class SplashScreenActivity extends Activity {
         final Thread splashScreen=new Thread(){
             public void run(){
                 try{
-                    sleep(3000);
+                    sleep(1000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }
